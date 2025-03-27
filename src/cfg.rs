e89bd684d32738f82aee51ef495b77214b9ffaa8
Nilando <nilandschumacher@gmail.com>
2025-03-26 17:44:44 -0700

    created a dfa executor

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,7 +1,9 @@
 use crate::tac::{Tac, TacFunc, LabelID, VarID};
+use std::ops::{Index, IndexMut};
 use crate::cfg_builder::CFGBuilder;
 
 pub type BlockID = usize;
+const ENTRY_BLOCK_ID: usize = 0;
 
 #[derive(Debug)]
 pub struct Block {
@@ -10,6 +12,7 @@ pub struct Block {
     pub label: Option<LabelID>,
     pub successors: Vec<BlockID>,
     pub predecessors: Vec<BlockID>,
+    pub entry_arguments: Option<Vec<VarID>>,
 }
 
 impl Block {
@@ -20,13 +23,14 @@ impl Block {
             code: vec![],
             predecessors: vec![],
             successors: vec![],
+            entry_arguments: None,
         }
     }
 }
 
 #[derive(Debug)]
 pub struct CFG {
-    blocks:  Vec<Block>,
+    pub blocks: Vec<Block>,
 }
 
 impl CFG {
@@ -36,5 +40,33 @@ impl CFG {
         Self {
             blocks
         }
+    }
+
+    pub fn get_block_ids(&self) -> Vec<BlockID> {
+        self.blocks.iter().map(|block| block.id).collect()
+    }
+    
+    pub fn get_exit_block_ids(&self) -> Vec<BlockID> {
+        self.blocks.iter()
+            .filter(|block| block.successors.is_empty())
+            .map(|block| block.id)
+            .collect()
+    }
+
+    pub fn get_entry_block_id(&self) -> BlockID {
+        ENTRY_BLOCK_ID
+    }
+}
+
+impl Index<BlockID> for CFG {
+    type Output = Block;
+    fn index<'a>(&'a self, i: usize) -> &'a Block {
+        &self.blocks[i]
+    }
+}
+
+impl IndexMut<BlockID> for CFG {
+    fn index_mut<'a>(&'a mut self, i: usize) -> &'a mut Block {
+        &mut self.blocks[i]
     }
 }
